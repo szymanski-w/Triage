59fa9bda01bcc1fa29d947eb1befa312037d22e7
Zoju <zojuuu@gmail.com>
2020-01-30 15:53:32 +0100

    Dodanie modelu i DataContext

---
+++ TriageApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using TriageApp.API.Models;
+
+namespace TriageApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
