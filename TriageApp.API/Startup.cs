29fb30ebf0db907abf960058d9e588b283640035
Zoju <zojuuu@gmail.com>
2020-02-19 15:18:22 +0100

    Wyświetlanie danych API na stronie

--- TriageApp.API/Startup.cs
+++ TriageApp.API/Startup.cs
@@ -29,6 +29,7 @@ namespace TriageApp.API
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConntection")));
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +45,8 @@ namespace TriageApp.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
             app.UseEndpoints(endpoints =>
             {
