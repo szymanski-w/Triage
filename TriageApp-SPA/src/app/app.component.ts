25b7ddd1e845586dd612880181ae2a3cd4218525
Zoju <zojuuu@gmail.com>
2020-02-21 10:56:30 +0100

    nav.component

--- TriageApp-SPA/src/app/app.component.ts
+++ TriageApp-SPA/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TriageApp - SPA';
+  title = 'Simple Triage Assistent';
 }
--- TriageApp.API/Startup.cs
+++ TriageApp.API/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore; // .Sqlite    - nugat package
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
